9c079e2bce145cecccf691594fd479bf9cc9d780
alexk <alexk@ALEX-DESKTOP>
2025-05-01 23:12:44 +1200

    Added functionality to Share To-Do Lists

--- ToDoListAppA2/Controllers/MyToDoListsController.cs
+++ ToDoListAppA2/Controllers/MyToDoListsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 using ToDoListAppA2.Data;
 using ToDoListAppA2.Models;
 
@@ -142,6 +143,76 @@ namespace ToDoListAppA2.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ToDoList/Share
+        public async Task<IActionResult> Share(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDoList = await _context.ToDoLists
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (toDoList == null)
+            {
+                return NotFound();
+            }
+
+            return View(toDoList);
+        }
+
+        // POST: ToDoList/Share
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Share(int id, string email)
+        {
+            var toDoList = await _context.ToDoLists.FirstOrDefaultAsync(t => t.Id == id);
+            if (toDoList == null)
+            {
+                return NotFound();
+            }
+
+            // Check if email is null
+            if (string.IsNullOrEmpty(email))
+            {
+                // Error messages for user feedback, sent to <span> in view
+                ModelState.AddModelError("Email", "The Email field is required.");
+                return View(toDoList);
+            }
+
+            // Check if user with email exists
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "Could not find a User with that Email.");
+                return View(toDoList);
+            }
+
+            // Check if the To-Do list has already been shared with an inputted email
+            var isAlreadySharedWithUser = await _context.ToDoListShares
+               .AnyAsync(s => s.ToDoListId == id && s.SharedWithUserId == user.Id);
+
+            if (isAlreadySharedWithUser)
+            {
+                ModelState.AddModelError("Email", "This To-Do List is already shared with that User.");
+                return View(toDoList);
+            }
+
+            // If checks pass, create a new ToDoListShare object
+            var toDoListShare = new ToDoListShare
+            {
+                ToDoListId = id,
+                SharedWithUserId = user.Id
+            };
+
+            // Add new ToDoListShare to database
+            _context.ToDoListShares.Add(toDoListShare);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
 
         private bool ToDoListExists(int id)
