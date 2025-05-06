9f39d9d396969959bd8661af9283a0bb6716a204
WillMark12544 <williamjosephmark@gmail.com>
2025-05-06 13:00:50 +1200

    UI Changes for Homepage, Login, Register, Forgot Password ect

--- ToDoListAppA2/obj/Debug/net8.0/ToDoListAppA2.AssemblyInfo.cs
+++ ToDoListAppA2/obj/Debug/net8.0/ToDoListAppA2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bec4c4c7d1a35ed26a278dc71b1e9ea3a52a34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8820abb0fba21cf08c4bda99ae3292d87e1f663d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
