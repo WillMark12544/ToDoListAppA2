8251b2e9c169b3d9981fe9e7af481c585163bbcd
alexk <alexk@ALEX-DESKTOP>
2025-05-27 13:56:05 +1200

    UX/UI improvement to Logout page + Looks prettier and less basic now! + Also shows which user is being logged out

--- ToDoListAppA2/obj/Debug/net8.0/ToDoListAppA2.AssemblyInfo.cs
+++ ToDoListAppA2/obj/Debug/net8.0/ToDoListAppA2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05358d28ecf078ccfc1c352453d0bc60e51d401d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0064deef5b5b7d5f23ad14a4b276fa2071eae69")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
