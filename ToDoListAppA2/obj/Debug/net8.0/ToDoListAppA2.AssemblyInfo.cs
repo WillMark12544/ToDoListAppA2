44352f978989919311e450ca791fa59ce5ceb83c
WillMark12544 <williamjosephmark@gmail.com>
2025-05-29 11:27:22 +1200

    Fixed Admin being able to change his own role

--- ToDoListAppA2/obj/Debug/net8.0/ToDoListAppA2.AssemblyInfo.cs
+++ ToDoListAppA2/obj/Debug/net8.0/ToDoListAppA2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65d340837eeeb064f3aad2cc9b9607e42926cef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3035568b6b39009d47c02ab40c893b71cc84054")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListAppA2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
